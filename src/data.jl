b5b4c490797859bf45a955b89e827adfadefc422
Jonas Hörsch <jonas.hoersch@kit.edu>
2018-07-23 19:25:59 +0200

    Add thin-layered pypsa import

--- src/data.jl
+++ src/data.jl
@@ -1,13 +1,10 @@
 using NCDatasets
 
-using DataFrames
-using CSV
+abstract type AbstractNcData <: Data end
+
+include("data/pypsa.jl")
 
 struct NcData <: Data
-    dataset::Dataset
-end
-
-struct PypsaNcData <: Data
     dataset::Dataset
 end
 
@@ -31,123 +28,17 @@ end
 # Base.show(io::IO, v::Variant) = for q=v print(io, "::"); show(io, q) end
 # Base.show(io::IO, q::Quantity) = print(io, string(q.name, q.isvariable ? "_v" : "", "_", join(string.(q.dimensions), ":")))
 
-function splitbyattr(ds, attr)
-    if !in(attr, ds)
-        return Dict(:general=>Colon())
-    end
+# or even type based as a variant of
 
-    da = ds[attr]
-    df = DataFrame(inds=1:length(da), class=da[:])
-    classes = Dict{Symbol, eltype(df[:inds])}()
-    for g = groupby(df, :class)
-        classes[g[1, :class]] = g[:inds]
-    end
-    classes
-end
+# struct QAxis{name,N} end
 
-"""
-    PypsaNc
-
-"""
-function PypsaNcData(ds)
-    # Needs to know about available components and their mapping
-    # generators -> Generator
-    # storage_units -> StorageUnit
-    # loads -> Load
-    # buses -> Bus (is a component?)
-    # lines -> Line
-    # links -> Link
-    # transformers -> Transformer
-    # SubNetwork is always there
-
-    # Check for available components by looking for size of _i coordinate
-    # For one component something like
-    pypsacomponents = CSV.read(joinpath(Pkg.dir("EnergyModels"), "src", "pypsa.components.csv"))
-
-    # Resolve julia types in the EnergyModels module
-    pypsacomponents[:componenttype] = getfield.(current_module(), Symbol.(pypsacomponents[:componenttype]))
-
-    # componenttypes = [c[:componenttype]
-    #                   for c = eachrow(pypsacomponents)
-    #                   if haskey(ds.dim, c[:listname]) && ds.dim[c[:listname]] > 0]
-
-    # Determine defined components and split them into classes
-    # Work In Progress: for now we assume that splitting on carrier is good enough
-    components = Component[]
-    splits_by_component = Dict{String}{Dict{Symbol}{Any}}()
-    mapping = Dict{String}{Dict{Symbol}{String}}()
-    for row = eachrow(pypsacomponents)
-        listname = row[:listname]
-        ctype = row[:componenttype]
-
-        if !haskey(ds.dim, listname) || ds.dim[listname] == 0
-            continue
-        end
-
-        splits = splitbyattr(ds, listname * "_carrier")
-        splits_by_component[naming(ctype)] = splits
-
-        # TODO read in mapping between pypsa component names and EnergyModels
-        CSV.read(joinpath(Pkg.dir("EnergyModels"), "src", string("pypsa.", listname, ".csv")))
-        
-
-        append!(components, ctype.(keys(splits)))
-    end
-
-
-
-
-
-    ## Below there is a try to figure out the splits based on grouping all
-    ## information that could change
-
-    # Classes should split on carrier and variants as defined (fex for generators) by:
-    # - generators_i
-    # - generators_p_nom_extendable
-    # - generators_t_p_max_pu_i
-    # - generators_t_p_min_pu_i
-    # - generators_committable
-
-    jtype = Generator
-    c = pypsacomponents[jtype]
-
-    c = @NT(jtype=EM.Generator,
-            listname="generators",
-            variantattrs=[:p_nom_extendable, :committable],
-            timedepattrs=[:p_max_pu, :p_min_pu, :marginal_cost])
-
-    # Build feature dataframe
-    df = DataFrame(name=as_data(ds[string(c.listname, "_i")]))
-    df[:inds] = 1:length(df)
-    for attr = c.variantattrs
-        key = string(c.listname, "_", attr)
-        if haskey(ds, key) df[attr] = as_data(ds[key], Vector{Bool}) end
-    end
-    for attr = c.timedepattrs
-        key = string(c.listname, "_t_", attr, "_i")
-        if haskey(ds, key)
-            df[attr] = false
-            df[attr][findin(df[:name], as_data(ds[key]))] = true
-        end
-    end
-    map(groupby(df, df.colindex.colnames[2:end])) do x
-        @NT(variant = squeeze(convert(Array{Bool}{2}, df[1,2:end]), 1),
-            index = df[:inds])
-    end
-
-    components = CSV.read(joinpath(@__DIR__, 'pypsa.generators.csv'))
-    Dict()
-    for c = classes, attr = attrs
-        inds = findin(ds[string(listname, "_t_", attr, "_i")], ds[string(listname, "_i")][c.inds])
-        if length(inds) == 0 continue end
-        @assert length(inds) == length(c.inds)
-
-    end
-end
+# struct Quantity{name,isvar,N,Ax}
+#     Quantity{name,isvar,N,Ax}() where {name,isvar<:Bool,N,Ax<:Tuple{Vararg{QAxis,N}}} = new{name,isvar,N,Ax}()
+# end
 
 function NcData(filename::String)
     ds = Dataset(filename)
-    (haskey(ds.attrs, "network_pypsa_version") ? PypsaNcData : NcData)(ds)
+    (haskey(ds.attrib, "network_pypsa_version") ? PypsaNcData : NcData)(ds)
 end
 
 function load(filename::String)::Data
@@ -158,23 +49,29 @@ function load(filename::String)::Data
     end
 end
 
-"""
-Read dataarray from netcdf file as AxisArray
-"""
-
-as_data(x) = nomissing(x[:])
-as_data(x, ::Type{T}) where T = convert(T, x)
-
-function as_axisarray(ds::Dataset, name::String)
-    da = ds[name]
-    AxisArray(as_data(da), (Axis{Symbol(n)}(as_data(ds[n]))
-                            for n=dimnames(da))...)
+naming(::T, symbols...) where T<:Component = join((naming(T), string.(symbols)...), "::")
+function naming(::Type{T}) where T<:Component
+    s = lowercase(string(T))
+    s[findlast(s, '.')+1:end] * (s[end] == 's' ? "es" : "s")
 end
 
-naming(component::T) where T<:Component = lowercase(string(T, "s"))
-
-function naming(component::Component, class::Symbol, param::Symbol)
-    join(string.((naming(component), class, param)), "::")
+function Base.get(data::NcData, args...)
+    da = data.dataset[naming(args...)]
+    AxisArray(da[:], (axis(data, n) for n = dimnames(da))...)
 end
 
-Base.get(data::NcData, args...) = as_axisarray(data.dataset, naming(args...))
+axis(data::AbstractNcData, n::Symbol) = axis(data, String(n))
+axis(data::AbstractNcData, n::String) = Axis{Symbol(n)}(disallowmissing(data.dataset[n][:]))
+
+# Interface for the abstract type Data
+#
+# * Required methods
+#
+# Base.get(data, component, class, param)
+# returns an AxisArray of `param` from `component::class`
+#
+# components(data)
+# returns Array{DataType} with the described components
+#
+# classes(data, ::Type{T}) where T<:Component
+# returns Array{Symbol} of the described classes
---
+++ src/data/pypsa.jl
@@ -0,0 +1,192 @@
+using DataFrames
+using CSV
+
+struct PypsaAttrInfo
+    pypsaname::String
+    indices::Union{Colon,UnitRange{Int64},StepRange{Int64,Int64},Vector{Int64}}
+end
+
+struct PypsaClassInfo
+    class::Symbol
+    names::Vector{String}
+    attrinfos::Dict{Symbol,PypsaAttrInfo}
+    variables::Set{Symbol}
+end
+
+function maybe_as_range(x::Vector{T}) where T<:Number
+    if length(x) < 3 return x end
+    d = diff(x)
+    s = d[1]
+    if all(d .== s)
+        s == 1 ? x[1]:x[end] : x[1]:s:x[end]
+    else
+        x
+    end
+end
+
+function splitbyattr(ds, attr)
+    if !haskey(ds, attr)
+        return Dict(:undef=>Colon())
+    end
+
+    da = ds[attr]
+    df = DataFrame(indices=1:length(da), class=da[:])
+    Dict(Symbol(g[1, :class]) => maybe_as_range(g[:indices])
+         for g = groupby(df, :class))
+end
+
+function pypsaclassinfos(ds, listname)
+    classinfos = Dict{Symbol, PypsaClassInfo}()
+
+    attrs = CSV.read(joinpath(@__DIR__, string("pypsa.", listname, ".csv"));
+                     truestring="t", falsestring="f")
+    attrs[:time_dependent] = recode(attrs[:time_dependent], missing=>false)
+    attrs[:EM] = Symbol.(attrs[:EM])
+
+    for (class, indices) = splitbyattr(ds, listname * "_carrier")
+        names = disallowmissing(ds[listname * "_i"][indices])
+        attrinfos = Dict{Symbol}{PypsaAttrInfo}()
+        variables = Set{Symbol}()
+
+        for attr = eachrow(attrs)
+            attrname_t = string(listname, "_t_", attr[:PyPSA])
+            if attr[:time_dependent] && in(attrname_t * "_i", ds)
+                indices_t = findin(ds[attrname_t * "_i"], names)
+            else
+                indices_t = Int64[]
+            end
+
+            attrinfos[attr[:EM]] =
+                if length(indices_t) > 0
+                    @assert(length(indices_t) == length(names),
+                            "$listname::$class has static and time-dependent $(attr[:PyPSA])")
+                    PypsaAttrInfo(attrname_t, indices_t)
+                else
+                    PypsaAttrInfo(string(listname, "_", attr[:PyPSA]), indices)
+                end
+        end
+
+        if haskey(attrs, :variable_switch)
+            for attr = eachrow(@views attrs[.!ismissing.(attrs[:variable_switch]), :])
+                switchname = string(listname, "_", attr[:variable_switch])
+                if !haskey(ds, switchname) continue end
+                isvariable = ds[switchname][indices]
+                if any(isvariable)
+                    @assert all(isvariable) "$(attr[:variable_switch]) of $listname::$class must either be true or false (not both)"
+                    push!(variables, attr[:EM])
+                end
+            end
+        end
+
+        classinfos[class] = PypsaClassInfo(class, names, attrinfos, variables)
+    end
+
+    classinfos
+end
+
+"""
+    PypsaNcData
+
+"""
+struct PypsaNcData <: AbstractNcData
+    dataset::Dataset
+    components::Vector{DataType}
+    classinfos::Dict{String}{Dict{Symbol}{PypsaClassInfo}}
+end
+
+function Base.show(io::IO, data::PypsaNcData)
+    println(io, string(typeof(data)), " based on '", path(data.dataset), "' describes")
+    for c = data.components[1:end]
+        ci = data.classinfos[naming(c)]
+        classes = (string(cl.class, " (", length(cl.names), ")") for cl = values(ci))
+        println(io, "    ", naming(c), ": ", join(classes, ", "))
+    end
+end
+
+function PypsaNcData(ds)
+    # Check for available components by looking for size of _i coordinate
+    # For one component something like
+    pypsacomponents = CSV.read(joinpath(@__DIR__, "pypsa.components.csv"))
+
+    # Determine defined components and split them into classes
+    # Work In Progress: for now we assume that splitting on carrier is good enough
+    components = DataType[]
+    classinfos = Dict{String}{Dict{Symbol}{PypsaClassInfo}}()
+    for row = eachrow(pypsacomponents)
+        listname = row[:listname]
+
+        # Resolve julia types in the EnergyModels module (i'm not sure if that's
+        # the most sensible way to do it)
+        ctype = getfield(EnergyModels, Symbol.(row[:componenttype]))
+
+        if !haskey(ds.dim, listname * "_i") || ds.dim[listname * "_i"] == 0
+            continue
+        end
+
+        push!(components, ctype)
+        classinfos[naming(ctype)] = pypsaclassinfos(ds, listname)
+    end
+
+    # ## Below there is a try to figure out the splits based on grouping all
+    # ## information that could change
+
+    # # Classes should split on carrier and variants as defined (fex for generators) by:
+    # # - generators_i
+    # # - generators_p_nom_extendable
+    # # - generators_t_p_max_pu_i
+    # # - generators_t_p_min_pu_i
+    # # - generators_committable
+
+    # jtype = Generator
+    # c = pypsacomponents[jtype]
+
+    # c = @NT(jtype=EM.Generator,
+    #         listname="generators",
+    #         variantattrs=[:p_nom_extendable, :committable],
+    #         timedepattrs=[:p_max_pu, :p_min_pu, :marginal_cost])
+
+    # # Build feature dataframe
+    # df = DataFrame(name=as_data(ds[string(c.listname, "_i")]))
+    # df[:inds] = 1:length(df)
+    # for attr = c.variantattrs
+    #     key = string(c.listname, "_", attr)
+    #     if haskey(ds, key) df[attr] = as_data(ds[key], Vector{Bool}) end
+    # end
+    # for attr = c.timedepattrs
+    #     key = string(c.listname, "_t_", attr, "_i")
+    #     if haskey(ds, key)
+    #         df[attr] = false
+    #         df[attr][findin(df[:name], as_data(ds[key]))] = true
+    #     end
+    # end
+    # map(groupby(df, df.colindex.colnames[2:end])) do x
+    #     @NT(variant = squeeze(convert(Array{Bool}{2}, df[1,2:end]), 1),
+    #         index = df[:inds])
+    # end
+
+    PypsaNcData(ds, components, classinfos)
+end
+
+function Base.get(data::PypsaNcData, component::Component, class::Symbol, param::Symbol)
+    name = naming(component)
+    classinfo = data.classinfos[name][class]
+    attrinfo = classinfo.attrinfos[param]
+
+    da = data.dataset[attrinfo.pypsaname]
+    dims = dimnames(da)[2:end]
+
+    # Benchmarking shows that getting the full array first and then
+    # subsetting is faster even if we need to get only 1 or 2 lines
+    # We would need to improve NCDatasets first!
+    AxisArray(da[:][attrinfo.indices,ntuple(i->:,length(dims))...],
+              Axis{Symbol(name)}(classinfo.names),
+              (axis(data, n) for n = dims)...)
+end
+
+function axis(data::PypsaNcData, c::Component, class)
+    name = naming(c)
+    Axis{Symbol(name)}(data.classinfos[name][class].names)
+end
+
+components(data::PypsaNcData) = data.components
+classes(data::PypsaNcData, T) = keys(data.classinfos[naming(T)])
