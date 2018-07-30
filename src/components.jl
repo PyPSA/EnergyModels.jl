3d6529bb5f9dd8264433ed482cd1a1d6997fd852
Jonas Hörsch <jonas.hoersch@kit.edu>
2018-07-30 16:00:00 +0200

    components: Switch to simpler definitions of components
    Using the compat addition to split bounds into anonymous constraints
    automatically.

--- src/components.jl
+++ src/components.jl
@@ -45,63 +45,41 @@ axis(m::EnergyModel, args...) = axis(m.data, args...)
 axis(c::Component) = axis(c.model, c, c.class)
 axis(c::Component, attr) = axis(c.model, attr)
 
-"""
-    relativeaxis(args...)
-
-Return an Iterator which allows for relative operations like i-1 or i+1 to refer
-to the next index. Beware: It is slower by several orders of magnitude!
-"""
-relativeaxis(args...) = RelativeIterator(axis(args...))
-
-## Definition of a generator
-
+## Defaults for OnePortComponents
 cost(c::OnePortComponent) = sum(c[:marginal_cost] .* c[:p]) + sum(c[:capital_cost] .* c[:p_nom])
-
+add_nodal_balance!(balance, c::OnePortComponent) = add_nodal_balance!(balance, c[:bus], nodal_balance(c))
 nodal_balance(c::OnePortComponent) = c[:p]
-function build(m::EnergyModel, c::Generator)
-    T = axis(m, :snapshots)
+
+## Generator
+function build(c::Generator)
+    T = axis(c, :snapshots)
     G = axis(c)
 
     if isvar(c, :p_nom)
         @emvariable c c[:p_nom_min][g] <= p_nom[g=G] <= c[:p_nom_max][g]
-        @emvariable c p[g=G,t=T]
-        @emconstraint c p_lower[g=G,t=T] c[:p][g,t] >= c[:p_min_pu][g,t] * c[:p_nom][g]
-        @emconstraint c p_upper[g=G,t=T] c[:p][g,t] <= c[:p_max_pu][g,t] * c[:p_nom][g]
-    else
-        @emvariable c c[:p_min_pu][g,t] * c[:p_nom][g] <= p[g=G,t=T] <= c[:p_max_pu][g,t] * c[:p_nom][g]
     end
+
+    @emvariable c c[:p_min_pu][g,t] * c[:p_nom][g] <= p[g=G,t=T] <= c[:p_max_pu][g,t] * c[:p_nom][g]
 end
 
+## StorageUnit
 cost(c::StorageUnit) = sum(c[:marginal_cost] .* c[:p_dispatch]) + sum(c[:capital_cost] .* c[:p_nom])
 nodal_balance(c::StorageUnit) = c[:p_dispatch] .- c[:p_store]
-function build(m::EnergyModel, c::StorageUnit)
-    T = axis(m, :snapshots)
+function build(c::StorageUnit)
+    T = axis(c, :snapshots)
     S = axis(c)
 
     if isvar(c, :p_nom)
         @emvariable c c[:p_nom_min][s] <= p_nom[s=S] <= c[:p_nom_max][s]
-
-        @emvariable c p_dispatch[s=S,t=T] >= 0
-        @emconstraint(c, p_dispatch_upper[s=S,t=T],
-                      c[:p_dispatch][s,t] <= c[:p_max_pu][s,t] * c[:p_nom][s])
-
-        @emvariable c p_store[s=S,t=T] >= 0
-        @emconstraint(c, p_store_upper[s=S,t=T],
-                      c[:p_store][s,t] <= - c[:p_min_pu][s,t] * c[:p_nom][s])
-
-        @emvariable c state_of_charge[s=S,t=T] >= 0
-        @emconstraint(c, state_of_charge_upper[s=S,t=T],
-                      c[:state_of_charge][s,t] <= c[:max_hours][s,t] * c[:p_nom][s])
-    else
-        @emvariable c 0 <= p_dispatch[s=S,t=T] <= c[:p_max_pu][s,t] * c[:p_nom][s]
-        @emvariable c 0 <= p_store[s=S, t=T] <= - c[:p_min_pu][s,t] * c[:p_nom][s]
-        @emvariable c 0 <= state_of_charge[s=S,t=T] <= c[:max_hours][s,t] * c[:p_nom][s]
     end
 
+    @emvariable c 0 <= p_dispatch[s=S,t=T] <= c[:p_max_pu][s,t] * c[:p_nom][s]
+    @emvariable c 0 <= p_store[s=S, t=T] <= - c[:p_min_pu][s,t] * c[:p_nom][s]
+    @emvariable c 0 <= state_of_charge[s=S,t=T] <= c[:max_hours][s,t] * c[:p_nom][s]
     @emvariable c 0 <= spill[s=S,t=T] <= c[:inflow][s,t]
 
-    soc_prev = roll(c[:state_of_charge], :storageunits=>1)
-    if !c[:cyclic_state_of_charge] soc_prev[:,1] = c[:state_of_charge_initial] end
+    soc_prev = circshift(c[:state_of_charge], :snapshots=>1)
+    if !c[:cyclic_state_of_charge] soc_prev[:,T[1]] .= c[:state_of_charge_initial] end
 
     @emconstraint(c, soc_eq[s=S, t=T],
                   c[:state_of_charge][s,t] - soc_prev[s,t]
@@ -110,48 +88,67 @@ function build(m::EnergyModel, c::StorageUnit)
                   + c[:inflow][s,t] - c[:spill][s,t])
 end
 
+## Store
 cost(c::Store) = sum(c[:marginal_cost] .* c[:p]) + sum(c[:capital_cost] .* c[:e_nom])
-nodal_balance(c::Store) = c[:p]
-function build(m::EnergyModel, c::Store)
-    T = axis(m, :snapshots)
+function build(c::Store)
+    T = axis(c, :snapshots)
     S = axis(c)
 
     if isvar(c, :e_nom)
         @emvariable c c[:e_nom_min][s] <= e_nom[s=S] <= c[:e_nom_max][s]
-
-        @emvariable c e[s=S,t=T]
-        @emconstraint(c, e_upper[s=S,t=T], c[:e][s,t] <= c[:e_max_pu][s,t] * c[:e_nom][s])
-        @emconstraint(c, e_lower[s=S,t=T], c[:e][s,t] >= c[:e_min_pu][s,t] * c[:e_nom][s])
-    else
-        @emvariable c 0 <= e_dispatch[s=S,t=T] <= c[:e_max_pu][s,t] * c[:e_nom][s]
-        @emvariable c 0 <= e_store[s=S, t=T] <= - c[:e_min_pu][s,t] * c[:e_nom][s]
     end
 
+    @emvariable c c[:e_min_pu][s,t] * c[:e_nom][s] <= e[s=S,t=T] <= c[:e_max_pu][s,t] * c[:e_nom][s]
     @emvariable c p[s=S,t=T]
 
-    e_prev = rol(c[:e], :stores=>1)
-    if !c[:e_cyclic] e_prev[:,S[1]] = c[:e_initial] end
+    e_prev = circshift(c[:e], :snapshots=>1)
+    if !c[:e_cyclic] e_prev[:,T[1]] .= c[:e_initial] end
 
     @emconstraint(c, e_eq[s=S, t=T],
-                  c[:e][s,t] - (1. - c[:standing_loss]) * e_prev[s,t] == c[:p])
+                  c[:e][s,t] - (1. - c[:standing_loss][s]) * e_prev[s,t] == c[:p])
 end
 
-
+## Load
 cost(c::Load) = 0.
 nodal_balance(c::Load) = - c[:p_set]
 build(c::Load) = nothing
 
+
+## Default for BranchComponents
+function add_nodal_balance!(balance, c::BranchComponent)
+    add_nodal_balance!(balance, c[:bus0], -c[:p])
+    add_nodal_balance!(balance, c[:bus1], c[:p])
+end
+
+## Link
+function add_nodal_balance!(balance, c::Link)
+    add_nodal_balance!(balance, c[:bus0], -c[:p])
+    add_nodal_balance!(balance, c[:bus1], c[:efficiency].*c[:p])
+end
 cost(c::Link) = sum(c[:marginal_cost] .* c[:p]) + sum(c[:capital_cost] .* c[:p_nom])
-function build(m::EnergyModel, c::Link)
-    T = axis(m, :snapshots)
+function build(c::Link)
     L = axis(c)
 
     if isvar(c, :p_nom)
         @emvariable c c[:p_nom_min][l] <= p_nom[l=L] <= c[:p_nom_max][l]
-        @emvariable c p[l=L,t=T]
-        @emconstraint c p_upper[l=L,t=T] p[l,t] <= c[:p_max_pu][l,t] * c[:p_nom][l]
-        @emconstraint c p_lower[l=L,t=T] p[l,t] >= - c[:p_min_pu][l,t] * c[:p_nom][l]
-    else
-        @emvariable c c[:p_min_pu][l,t] * c[:p_nom][l] <= p[l=L,t=T] <= c[:p_max_pu][l,t] * c[:p_nom][l]
     end
+
+    @emvariable c c[:p_min_pu][l,t] * c[:p_nom][l] <= p[l=L,t=:snapshots] <= c[:p_max_pu][l,t] * c[:p_nom][l]
 end
+
+## Line
+cost(c::Line) = sum(c[:marginal_cost] .* c[:p]) + sum(c[:capital_cost] .* c[:s_nom])
+function add_nodal_balance!(balance, c::Line)
+    add_nodal_balance!(balance, c[:bus0], c[:p])
+    add_nodal_balance!(balance, c[:bus1], -c[:p])
+end
+function build(c::Line)
+    L = axis(c)
+
+    if isvar(c, :p_nom)
+        @emvariable c c[:s_nom_min][l] <= s_nom[l=L] <= c[:s_nom_max][l]
+    end
+
+    @emvariable c -c[:s_max_pu][l,t] * c[:s_nom][l] <= p[l=L,t=:snapshots] <= c[:s_max_pu][l,t] * c[:s_nom][l]
+end
+
