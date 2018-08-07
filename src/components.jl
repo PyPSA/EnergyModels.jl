259df57355b6f1f68522086fe4add0cceda8fe3a
Jonas Hörsch <jonas.hoersch@kit.edu>
2018-08-07 21:32:00 +0200

    components: Add build for EnergyModel, impedances for lines and transformers

--- src/components.jl
+++ src/components.jl
@@ -113,6 +113,22 @@ cost(c::OnePort) = sum(c[:marginal_cost] .* c[:p]) + sum(c[:capital_cost] .* c[:
 add_nodal_balance!(balance, c::OnePort) = add_nodal_balance!(balance, c[:bus], nodal_balance(c))
 nodal_balance(c::OnePort) = c[:p]
 
+function build(m::EnergyModel)
+    info("* Equations for individual components")
+    for c = components(m)
+        isa(c, Bus) && continue
+        info("  - ", c.class, " (", naming(c), ")")
+        build(m, c)
+    end
+    info("* Equations for energy balance")
+    for c = components(m, Bus)
+        build(m, c)
+    end
+    info("* Equations for subnetworks")
+    for sn = m.subnetworks
+        build(m, sn)
+    end
+end
 
 ## Generator
 function build(c::Generator)
@@ -151,6 +167,14 @@ function build(c::StorageUnit)
                   - c[:p_dispatch][s,t] / c[:efficiency_dispatch][s]
                   + c[:inflow][s,t] - c[:spill][s,t])
 end
+
+# add_component(StorageUnit, "storageunits", Axes(:S, :T=>:snapshots), joinpath(@__DIR__, "storageunits.csv"))
+# VarParam(:p_nom, 0, :S), Param(:p_nom_min, 0, :S), Param(:p_nom_max, Inf, :S),
+# Variable(:p, 0, :S, :T), Param(:p_min_pu, -1, :S, :T), Param(:p_max_pu, 1, :S, :T),
+# Param(:efficiency_store, 1, :S, :T), Param(:efficiency_dispatch, 1, :S, :T),
+# Param(:standing_loss, 0, :S, :T), Param(:efficiency_dispatch, 1, :S, :T),
+
+
 
 ## Store
 cost(c::Store) = sum(c[:marginal_cost] .* c[:p]) + sum(c[:capital_cost] .* c[:e_nom])
@@ -195,9 +219,81 @@ function build(c::Link)
     @emvariable c c[:p_min_pu][l,t] * c[:p_nom][l] <= p[l=L,t=:snapshots] <= c[:p_max_pu][l,t] * c[:p_nom][l]
 end
 
-## Line
-cost(c::Line) = sum(c[:marginal_cost] .* c[:p]) + sum(c[:capital_cost] .* c[:s_nom])
-function add_nodal_balance!(balance, c::Line)
+## Lines and Transformers
+effectiveimpedance(c::PassiveBranch) = impedance(c)[c[:carrier] == "DC" ? :r_pu_eff : :x_pu_eff]
+phaseshift(c::PassiveBranch) = 0.
+
+function impedance(c::Line)
+    ax = axis(c)
+    bus_v_nom = c.model[Bus][:v_nom]
+    v_nom = bus_v_nom[indexin(c[:bus0], first(axisvalues(bus_v_nom)))]
+
+    p = gettypeparams(c.model.data, c, c.class)
+
+    num_parallel = c[:num_parallel]
+    length = c[:length]
+
+    # line types
+    x_pu = AxisArray(p[:x_per_length] ./ v_nom.^2 .* length ./ num_parallel, ax)
+    r_pu = AxisArray(p[:r_per_length] ./ v_nom.^2 .* length ./ num_parallel, ax)
+    b_pu = AxisArray(2pi*1e-9*p[:f_nom]*p[:c_per_length] .* v_nom.^2 .* length .* num_parallel, ax)
+
+    Dict(:x_pu => x_pu, :x_pu_eff => x_pu,
+         :r_pu => r_pu, :r_pu_eff => r_pu,
+         :b_pu => b_pu, :g_pu => 0)
+end
+
+phaseshift(c::Transformer) = gettypeparams(c.model.data, c, class)[:phase_shift]
+function impedance(c::Transformer)
+    p = gettypeparams(c.model.data, c, c.class)
+
+    num_parallel = c[:num_parallel]
+
+    r_pu = p[:vscr]/100 / p[:s_nom] ./ num_parallel
+    x_pu = sqrt(p[:vsc]^2 - p[:vscr]^2)/100 / p[:s_nom] ./ num_parallel
+    g_pu = p[:pfe]/1000 .* num_parallel
+    b_pu = (-sqrt(clamp((p[:i0] * p[:s_nom])^2 - (p[:pfe]/10)^2, 0., Inf)) / 100
+            .* num_parallel)
+
+    """Follows http://home.earthlink.net/~w6rmk/math/wyedelta.htm"""
+    function wye_to_delta(z1,z2,z3)
+        summand = z1*z2 + z2*z3 + z3*z1
+        (summand/z2,summand/z1,summand/z3)
+    end
+
+    """Convert given T-model parameters to PI-model parameters using wye-delta transformation"""
+    function apply_transformer_t_model!(r_pu, x_pu, g_pu, b_pu)
+        for i in eachindex(r_pu, x_pu, g_pu, b_pu)
+            z_series = r_pu[i] + im*x_pu[i]
+            y_shunt  = g_pu[i] + im*b_pu[i]
+
+            if y_shunt == 0 continue end
+
+            za, zb, zc = wye_to_delta(z_series/2, z_series/2, 1/y_shunt)
+
+            r_pu[i] = real(zc)
+            x_pu[i] = imag(zc)
+            g_pu[i] = real(2/za)
+            b_pu[i] = imag(2/za)
+        end
+    end
+
+    c[:model] == "t" && apply_transformer_t_model!(r_pu, x_pu, g_pu, b_pu)
+
+    tap_ratio = 1 .+ (c[:tap_position] .- p[:tap_neutral]) .* (p[:tap_step]/100.)
+    Dict(:r_pu => r_pu, :r_pu_eff => r_pu .* tap_ratio,
+         :x_pu => x_pu, :x_pu_eff => x_pu .* tap_ratio,
+         :g_pu => g_pu, :b_pu => b_pu,
+         :tap_ratio => tap_ratio,
+         :phase_shift => p[:phase_shift],
+         :s_nom => p[:s_nom])
+end
+
+
+cost(c::PassiveBranch) = sum(c[:marginal_cost] .* c[:p]) + sum(c[:capital_cost] .* c[:s_nom])
+nodal_balance(c::PassiveBranch) = @embalance(c, c[:bus0][b] => c[:p][b], c[:bus1][b] => -c[:p][b])
+
+function add_nodal_balance!(balance, c::PassiveBranch)
     add_nodal_balance!(balance, c[:bus0], c[:p])
     add_nodal_balance!(balance, c[:bus1], -c[:p])
 end
@@ -216,7 +312,7 @@ function build(m::EnergyModel, c::Bus)
     T = axis(m, :snapshots)
     B = axis(c)
     bal = nodal_balance(m, B)
-    @emconstraint(c, balance[B,T], bal .== 0)
+    @emconstraint(c, balance[b=B,t=T], bal[b,t] == 0)
 end
 
 function build(sn::SubNetwork)
