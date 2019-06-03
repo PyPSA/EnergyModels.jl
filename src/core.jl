77b87d6e36d3f1122e8afcd39485b423a6f0dbd6
Jonas Hoersch <coroa@posteo.de>
2019-06-03 17:17:10 +0200

    core: Fix convoluted type-inheritance around Containers
    - Rename `ModelElement` -> `Element`
    - `AbstractEnergyModel :> EnergyModel`
    - ```
      Element :> SubNetwork
              :> Bus
              :> Component
      ```

--- src/core.jl
+++ src/core.jl
@@ -7,41 +7,38 @@ abstract type Emission <: ExpressionType end
 struct Cost <: ExpressionType end
 struct CO2 <: Emission end
 
-"Can be `build` into a linked `jumpmodel`, they CAN have `variables` and `constraints`"
-abstract type ModelElement end
+"Can be `add`ed to a JuMP model, must have an `objects` dictionary and a `class`"
+abstract type Element end
 
-"Holds `components` and provides getindex access by their class or a `ContainerView`"
-abstract type Container <: ModelElement end
-
-abstract type AbstractModel <: Container end
+abstract type AbstractEnergyModel end
 
 "Represents a synchronuous zone. Induced by determine_subnetworks!(model)"
-struct SubNetwork{T <: AbstractModel} <: Container
+struct SubNetwork{T <: AbstractEnergyModel} <: Element
     model::T
     name::Symbol
     buses::Axis
 end
 
 "Connection points at which energy balance is upheld, has `axis` and `build`"
-struct Bus{T <: AbstractModel} <: ModelElement
+struct Bus{T <: AbstractEnergyModel} <: Element
     model::T
     name::Symbol
 end
 
 "Connected to at least one `Bus`. Additionally to `build` provides `nodalbalance` and `cost`"
-abstract type Component <: ModelElement end
+abstract type Component <: Element end
 
-mutable struct EnergyModel <: AbstractModel
+mutable struct EnergyModel
     components::Dict{Symbol,Component}
     subnetworks::Dict{Symbol,SubNetwork{EnergyModel}}
     buses::Dict{Symbol,Bus{EnergyModel}}
     data::Data
-    jump::Model
+    jumpmodel::Union{JuMP.AbstractModel,Nothing}
 end
 
 
 EnergyModel(filename::String; kwargs...) = EnergyModel(load(filename); kwargs...)
-EnergyModel(data::Data; solver=GurobiSolver()) = load(EnergyModel(Dict{Symbol,Component}(), Dict{Symbol,SubNetwork}(), Dict{Symbol,Bus}(), data, Model(solver=solver)))
+EnergyModel(data::Data) = load(EnergyModel(Dict{Symbol,Component}(), Dict{Symbol,SubNetwork}(), Dict{Symbol,Bus}(), data, nothing))
 
 function load(m::EnergyModel)
     for T = modelelements(m.data), class = classes(m.data, T) push!(m, T(m, class)) end
@@ -49,20 +46,20 @@ function load(m::EnergyModel)
     m
 end
 
+const Container = Union{SubNetwork{EnergyModel}, EnergyModel}
+
 Base.push!(m::EnergyModel, c::Component) = (m.components[c.class] = c)
 Base.push!(m::EnergyModel, c::SubNetwork) = (m.subnetworks[c.name] = c)
 Base.push!(m::EnergyModel, c::Bus) = (m.buses[c.name] = c)
 
 components(sn::SubNetwork) = components(model(sn))
-components(c::Container) = values(c.components)
+components(c::EnergyModel) = values(c.components)
 components(m::Container, T::Type{<:Component}) = (c for c = components(m) if isa(c, T))
-# containerview(m::EnergyModel) = ContainerView()
-# containerview(sn::SubNetwork) = SubContainerView()
 
 buses(m::EnergyModel) = values(m.buses)
 subnetworks(m::EnergyModel) = values(m.subnetworks)
 
-JuMP.solve(m::EnergyModel; kwargs...) = solve(m.jump; kwargs...)
+JuMP.optimize!(m::EnergyModel; kwargs...) = optimize!(m.jumpmodel; kwargs...)
 
 Base.show(io::IO, m::EnergyModel) = print(io, typeof(m), " with ", length(m.components), " components")
 Base.show(io::IO, c::Component) = print(io, typeof(c), " for class ", c.class)
@@ -73,33 +70,30 @@ Base.show(io::IO, c::Component) = print(io, typeof(c), " for class ", c.class)
 # end
 
 
-jumpmodel(e::ModelElement) = jumpmodel(model(e))
-jumpmodel(m::EnergyModel) = m.jump
+jumpmodel(e::Element) = jumpmodel(model(e))
+jumpmodel(m::EnergyModel) = m.jumpmodel
 
 model(m::EnergyModel) = m
-model(e::ModelElement) = e.model
+model(e::Element) = e.model
 
 naming(c::Component) = c.class
-naming(e::ModelElement) = e.name
-naming(e::ModelElement, args...) = Symbol(naming(e), flatten(("::", a) for a=args)...)
+naming(e::Element) = e.name
+naming(e::Element, args...) = Symbol(naming(e), flatten(("::", a) for a=args)...)
 
 Base.findall(pred::Base.Fix2{typeof(in), <:Axis}, c::Component) = intersect((findall(pred, c[attr]) for attr = busattributes(c))...)
 Base.findall(pred::Base.Fix2{typeof(in), <:Axis}, c::Bus) = findall(pred, axis(c).val)
 
-isvar(m::EnergyModel, e::ModelElement, attr::Symbol) = isvar(m.data, e, attr)
-isvar(e::ModelElement, attr::Symbol) = isvar(model(e), e, attr)
+isvar(m::EnergyModel, e::Element, attr::Symbol) = isvar(m.data, e, attr)
+isvar(e::Element, attr::Symbol) = isvar(model(e), e, attr)
 
-Base.getindex(e::ModelElement, attr::Symbol) = (r = getjump(e, attr)) !== nothing ? r : getparam(e, attr)
+Base.getindex(e::Element, attr::Symbol) = (r = getjump(e, attr)) !== nothing ? r : getparam(e, attr)
 
-function getjump(e::ModelElement, attr::Symbol)
-    jm = jumpmodel(e)
-    name = naming(e, attr)
-    haskey(jm.objDict, name) ? AxisArray(jm[name]) : nothing
-end
-getvariable(e::ModelElement, attr::Symbol) = getjump(e, attr)
-JuMP.getvalue(e::ModelElement, attr::Symbol) = getvalue(getjump(e, attr))
-JuMP.getdual(e::ModelElement, attr::Symbol) = getdual(getjump(e, attr))
-getparam(e::ModelElement, attr::Symbol) = get(model(e).data, e, attr)
+getjump(e::Element, attr::Symbol) = get(e.objects, attr, nothing)
+getvariable(e::Element, attr::Symbol) = getjump(e, attr)
+getconstraint(e::Element, attr::Symbol) = getjump(e, attr)
+JuMP.getvalue(e::Element, attr::Symbol) = getvalue.(getjump(e, attr))
+JuMP.getdual(e::Element, attr::Symbol) = getdual.(getjump(e, attr))
+getparam(e::Element, attr::Symbol) = get(model(e).data, e, attr)
 
 function Base.getindex(m::EnergyModel, class::Symbol)
     for t = (:components, :buses, :subnetworks)
@@ -113,15 +107,14 @@ Base.getindex(m::EnergyModel, ::Type{Bus}) = ContainerView(m, Dict(c.name=>c for
 Base.getindex(sn::SubNetwork, T::Type{<:Component}) = SubContainerView(model(sn), Dict(c.class=>c for c=components(sn, T)), sn.buses)
 Base.getindex(sn::SubNetwork, ::Type{Bus}) = SubContainerView(model(sn), Dict(c.name=>c for c=buses(model(sn))), sn.buses)
 
-Base.view(e::ModelElement, attr::Symbol, axes) = WrappedArray(e[attr], axes...)
+Base.view(e::Element, attr::Symbol, axes) = WrappedArray(e[attr], axes...)
 
 axis(m::EnergyModel, args...) = axis(m.data, args...)
-axis(e::ModelElement) = axis(model(e), e)
-axis(e::ModelElement, attr) = axis(model(e), attr)
-axis(m::Container, T::Type{<:ModelElement}) = axis(m[T])
-axis(m::EnergyModel, T::Type{<:ModelElement}) = axis(m[T])
+axis(e::Element) = axis(model(e), e)
+axis(e::Element, attr) = axis(model(e), attr)
+axis(m::Container, T::Type{<:Element}) = axis(m[T])
+axis(m::EnergyModel, T::Type{<:Element}) = axis(m[T])
 
 # Could be specialized to not have to retrieve the whole axis (on the other
 # hand, the axis should be cached, anyway)
-Base.length(e::ModelElement) = length(axis(e))
-
+Base.length(e::Element) = length(axis(e))
