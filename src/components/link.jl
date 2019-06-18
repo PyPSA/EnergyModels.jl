68d80b4a89b02aded5e3608cc819d20209be171d
Jonas Hoersch <coroa@posteo.de>
2019-06-18 23:10:28 +0200

    Add link.jl

--- src/components/link.jl
+++ src/components/link.jl
@@ -10,29 +10,47 @@ function p(d::Link)
 end
 
 cost(d::Link) = sum(d[:marginal_cost] .* AxisArray(d[:p])) + sum(d[:capital_cost] .* (AxisArray(d[:p_nom]) - getparam(d, :p_nom)))
-function addto!(jm::ModelView, m::EnergyModel, d::Link)
-    T = axis(m, :snapshots)
+function addto!(jm::ModelView, m::EnergyModel, d::Link{DF}) where {DDF, DF <: LinearExpansionForm{DF}}
+    addto!(jm, m, with_formulation(d, LinearExpansionInvestmentForm))
+    addto!(jm, m, with_formulation(d, LinearExpansionDispatchForm{DDF}))
+end
+
+function addto!(jm::ModelView, m::EnergyModel, d::Link{DF}) where DF <: LinearExpansionInvestmentForm
     L = axis(d)
 
+    p_nom_min = get(d, :p_nom_min, L)
+    p_nom_max = get(d, :p_nom_max, L)
+
+    @variable(jm, p_nom_min[l] <= p_nom[l=L] <= p_nom_max[l])
+end
+
+function addto!(jm::ModelView, m::EnergyModel, d::Link{DF}) where
+      {DDF, DF <: LinearExpansionDispatchForm{DDF}}
+
+    L = axis(m, d)
+    T = axis(m, :snapshots)
+
+    p_nom = get(d, :p_nom)
     p_min_pu = get(d, :p_min_pu, L, T)
     p_max_pu = get(d, :p_max_pu, L, T)
 
-    if isvar(d, :p_nom)
-        p_nom_min = get(d, :p_nom_min, L)
-        p_nom_max = get(d, :p_nom_max, L)
+    @variable(jm, p[l=L,t=T])
 
-        @variables jm begin
-            p_nom_min[l] <= p_nom[l=L] <= p_nom_max[l]
-            p[l=L,t=T]
-        end
-
-        @constraints jm begin
-            p_lower[l=L,t=T], p[l,t] >= p_min_pu[l,t] * p_nom[l]
-            p_upper[l=L,t=T], p[l,t] <= p_max_pu[l,t] * p_nom[l]
-        end
-    else
-        p_nom = get(d, :p_nom, L)
-        @variable(jm, p_min_pu[l,t] * p_nom[l] <= p[l=L,t=T] <= p_max_pu[l,t] * p_nom[l])
+    @constraints jm begin
+        p_lower[l=L,t=T], p[l,t] >= p_min_pu[l,t] * p_nom[l]
+        p_upper[l=L,t=T], p[l,t] <= p_max_pu[l,t] * p_nom[l]
     end
 end
 
+function addto!(jm::ModelView, m::EnergyModel, d::Link{DF}) where
+      {DF <: DispatchForm}
+
+    L = axis(m, d)
+    T = axis(m, :snapshots)
+
+    p_nom = get(d, :p_nom)
+    p_min_pu = get(d, :p_min_pu, L, T)
+    p_max_pu = get(d, :p_max_pu, L, T)
+
+    @variable(jm, p_min_pu[l,t] * p_nom[l] <= p[l=L,t=T] <= p_max_pu[l,t] * p_nom[l])
+end
