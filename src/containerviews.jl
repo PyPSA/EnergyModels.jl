0f14436e7b768f967b58cc670e5cd8f491db8960
Jonas Hoersch <coroa@posteo.de>
2019-08-01 16:32:40 +0200

    Update containerviews to work with simple components

--- src/containerviews.jl
+++ src/containerviews.jl
@@ -18,19 +18,31 @@ tupleaxisvals(c, A) = AxisArrays.CategoricalVector(tuple.(c.class, A.val))
 _maybesubset(cv::ContainerView, c, A) = A
 _maybesubset(cv::SubContainerView, c::Component, A) = A[findall(in(cv.buses), c), ntuple(i->:,ndims(A)-1)...]
 
-_viewaxis(cv::AbstractContainerView{T}, a) where T = Axis{Symbol(naming(T))}(a)
+_wrapaxis(cv::AbstractContainerView{T}, a) where T = Axis{Symbol(naming(T))}(a)
+function _wrapaxisarray(cv::AbstractContainerView, c::Component, a::AxisArray)
+    a = _maybesubset(cv, c, a)
+    ax = first(AxisArrays.axes(a))
+    AxisArray(a, _wrapaxis(cv, tupleaxisvals(c, ax)))
+end
+function _wrapaxisarray(cv::AbstractContainerView, c::Component, a::Union{Float64,Symbol})
+    ax = _maybesubset(cv, c, tupleaxisvals(c))
+    AxisArray(fill(a, length(ax)), _wrapaxis(cv, ax))
+end
 
 devices(cv::AbstractContainerView) = values(cv.devices)
 devices(cv::AbstractContainerView, T::Type{<:Device}) = (c for c = devices(cv) if isa(c, T))
 
-axis(cv::AbstractContainerView) = _viewaxis(cv, vcat((_maybesubset(cv, c, tupleaxisvals(c)) for c = devices(cv))...))
+axis(cv::AbstractContainerView) = _wrapaxis(cv, vcat((_maybesubset(cv, c, tupleaxisvals(c)) for c = devices(cv))...))
 axis(cv::AbstractContainerView, T::Type{<:Component}) = axis(m[T])
 
 # Base.cat(cv::AbstractContainerView, cs::Vector{<:Component}, as::Vector) = @consense(as, "Single values do not agree")
-function Base.cat(cv::AbstractContainerView, cs::Vector{<:Component}, as::Vector)
-    _viewaxisarray(c, a) = AxisArray(a, _viewaxis(cv, tupleaxisvals(c, first(AxisArrays.axes(a)))))
-    cat((_viewaxisarray(c, _maybesubset(cv, c, a)) for (c, a) = zip(cs, as))...; dims=1)
-end
+"""
+    cat(containerview, components, attrvalues)
+
+Combine the `attrvalues` for the `components` in a `containerview` to a single AxisArray.
+"""
+Base.cat(cv::AbstractContainerView, components::Vector{<:Component}, attrvalues::Vector) =
+    cat((_wrapaxisarray(cv, c, a) for (c, a) = zip(components, attrvalues))...; dims=1)
 
 mapcat(f::Function, cv::AbstractContainerView) = cat(cv, collect(devices(cv)), map(f, devices(cv)))
 
